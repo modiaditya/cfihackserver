c200fdbc1d80b8131f17f40d18b8861e211a089b
Sivakumar Loganathan <sloganathan@linkedin.com>
2014-05-10 06:38:09 -0700

    basic typeahead search for subtypes

--- public/scripts/sita_chart.js
+++ public/scripts/sita_chart.js
@@ -1,8 +1,13 @@
 SITA.Chart = function() {
 
-$.get( "/mock/chartmock.json", function(data) {
-  var chartData = new SITA.ChartUtil().parseDrilldown(data);
-  $('#container').highcharts(chartData);
-});
+  //TODO: /chart?type=state&name=gujarat&subtype=district
+  $.get( "/mock/chartmock.json", function(data) {
+    var chartUtil = new SITA.ChartUtil();
+    var chartData = chartUtil.parseDrilldown(data);
+    $('#container').highcharts(chartData);
+
+    chartUtil.setupTypeahead("Type a " + data.subtype.name + " name", data.subtype.values);
+
+  });
 
 }
